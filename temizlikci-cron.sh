f8d0896cd6927ab2cae7c12f2a9cd8448d2461f1
sedatabase <sedatyildiz@mail.com>
2024-01-08 11:28:40 +0300

    Update temizlikci-cron.sh

--- temizlikci-cron.sh
+++ temizlikci-cron.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 clear
 find /home/* -name backup-*.tar.gz -type f -ls -exec rm -f {} \;
-find /home/*/logs -name *.gz -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.tar.gz -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.gz -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.zip -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.rar -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.mp3 -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.mp4 -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.flv -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.avi -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.wmv -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.dat -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.exe -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.bat -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.psd -type f -ls -exec rm -f {} \;
-find /home/*/public_html -name *.cdr -type f -ls -exec rm -f {} \;
+find /home/* -name *.gz -type f -ls -exec rm -f {} \;
+find /home/* -name *.tar.gz -type f -ls -exec rm -f {} \;
+find /home/* -name *.gz -type f -ls -exec rm -f {} \;
+find /home/* -name *.zip -type f -ls -exec rm -f {} \;
+find /home/* -name *.rar -type f -ls -exec rm -f {} \;
+find /home/* -name *.mp3 -type f -ls -exec rm -f {} \;
+find /home/* -name *.mp4 -type f -ls -exec rm -f {} \;
+find /home/* -name *.flv -type f -ls -exec rm -f {} \;
+find /home/* -name *.avi -type f -ls -exec rm -f {} \;
+find /home/* -name *.wmv -type f -ls -exec rm -f {} \;
+find /home/* -name *.dat -type f -ls -exec rm -f {} \;
+find /home/* -name *.exe -type f -ls -exec rm -f {} \;
+find /home/* -name *.bat -type f -ls -exec rm -f {} \;
+find /home/* -name *.psd -type f -ls -exec rm -f {} \;
+find /home/* -name *.cdr -type f -ls -exec rm -f {} \;
